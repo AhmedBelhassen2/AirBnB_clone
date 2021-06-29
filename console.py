f723078300faebfdd2cca1e4462e7b653d2a2e09
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-29 12:08:45 +0000

    all

--- console.py
+++ console.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 'that contains the entry point of the command interpreter'
-import cmd 
-
+import cmd
+import sys
+from models.base_model import BaseModel
+import models
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 class HBNBCommand(cmd.Cmd):
     'cmd class'
@@ -13,6 +22,19 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         'EOF command to exit the program'
         return True
-    
+
+    def do_create(self, arg):
+        'Creates a new instance'
+    args = arg.split()
+    if len(args) == 0:
+        print("** class name missing **")
+    if args[0] in classes:
+        new_dict = self._key_value_parser(args[1:])
+        instance = classes[args[0]](**new_dict)
+    else:
+        print("** class doesn't exist **")
+    print(instance.id)
+    instance.save()
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -5,9 +5,7 @@ Contains the Base class for the AirBnB clone.
 
 import uuid
 from datetime import datetime
-from models import storage
-
-
+import models
 class BaseModel:
 
     """Class for base model of object hierarchy."""
@@ -33,7 +31,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a human-readable string representation
@@ -47,7 +45,7 @@ class BaseModel:
         with the current datetime."""
 
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
