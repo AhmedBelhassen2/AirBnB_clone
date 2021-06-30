4e74ac40825c843213a4d580f2c682ce8cbf5406
lamine <youssejemli@gmail.com>
2021-06-30 12:04:52 +0100

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Module for Base class
-Contains the Base class for the AirBnB clone.
+Contains the Base class for the AirBnB clone console.
 """
 
 import uuid
 from datetime import datetime
-import models
+from models import storage
 
 
 class BaseModel:
@@ -33,7 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """Returns a human-readable string representation
@@ -47,7 +47,7 @@ class BaseModel:
         with the current datetime."""
 
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
