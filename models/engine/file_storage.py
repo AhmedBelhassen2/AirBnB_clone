bd5fe3b302a2f11c2ee0c0d969e470437d9a6b7e
lamine <youssejemli@gmail.com>
2021-06-30 16:33:49 +0100

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,43 +1,53 @@
 #!/usr/bin/python3
-
-'Contains the FileStorage class'
-
+"""Module for FileStorage class."""
+import datetime
 import json
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models.amenity import Amenity
+import os
 
 
 class FileStorage:
-    'that serializes instances to a JSON file and deserialize'
 
+    """Class for serializtion and deserialization of base classes."""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        'returns the dictionary'
+        """Returns __objects dictionary."""
+        # TODO: should this be a copy()?
         return FileStorage.__objects
 
     def new(self, obj):
-        'sets in __objects the obj with key <obj class name>.id'
-        if obj is not None:
-            key = obj.__class__.__name__ + "." + obj.id
-            self.__objects[key] = obj
+        """Sets new obj in __objects dictionary."""
+        # TODO: should these be more precise specifiers?
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        'serializes __objects to the JSON file (path: __file_path)'
-        json_objects = {}
-        for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, 'w') as f:
-            json.dump(json_objects, f)
+        """Serialzes __objects to JSON file."""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
 
     def reload(self):
-<<<<<<< HEAD
         """Deserializes JSON file into __objects."""
         if not os.path.isfile(FileStorage.__file_path):
             return
@@ -85,13 +95,3 @@ class FileStorage:
                          "text": str}
         }
         return attributes
-=======
-        'deserializes the JSON file to __objects'
-        try:
-            with open(self.__file_path, 'r') as f:
-                obj_js = json.load(f)
-            for key, value in obje_js.items():
-                self.__objects[key] = eval(key.split('.')[0])(**value)
-        except:
-            pass
->>>>>>> 5e5b30501ad8f34fd55315224d82c4c646a74383
